6f4c24ea0f6e1908ba4a5051789666402bf20e60
Vynicios Raphael Silva de Souza <vinnyrafael015@hotmail.com>
2025-12-16 14:32:35 -0300

    criação das routes e controllers

---
+++ routes/routes.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const router = express.Router();
+
+// referência a controllers que serão utilizados nas rotas
+// const ProdutosController = require("../controllers/produto");
